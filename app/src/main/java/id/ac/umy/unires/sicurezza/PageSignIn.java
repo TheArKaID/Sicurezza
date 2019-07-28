16090ad6da43263b0214177fa1e9a749329e4e24
Muhammad Miftah <muhammadmiftah998@gmail.com>
2019-07-28 14:41:38 +0700

    SignInPage

---
+++ app/src/main/java/id/ac/umy/unires/sicurezza/PageSignIn.java
@@ -0,0 +1,13 @@
+package id.ac.umy.unires.sicurezza;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PageSignIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page_sign_in);
+    }
+}
