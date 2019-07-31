143f1ee764007b5442212e7f4430658ebe62100c
Arifia Kasastra R <arka.progammer@gmail.com>
2019-08-01 00:36:05 +0700

    Add ServerAPI.java file

---
+++ app/src/main/java/id/ac/umy/unires/sicurezza/utils/ServerAPI.java
@@ -0,0 +1,11 @@
+package id.ac.umy.unires.sicurezza.utils;
+
+public class ServerAPI {
+    public final static String URL = "https://divman.000webhostapp.com/";
+    public final static String CekStatusKoURL = "cek-status.php";
+    public final static String LoginURL = "proses-login.php";
+    public final static String CekTengKoURL = "cek-tengko.php";
+    public final static String CekResidentURL = "cek-resident.php";
+    public final static String CekDetailResidentURL = "cek-detail.php";
+    public final static String TambahPoinURL = "tambah-poin.php";
+}
