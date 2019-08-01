4fb3b4fb3951a96db0c472db713e0d303f3fc408
Muhammad Miftah <muhammadmiftah998@gmail.com>
2019-08-01 20:40:08 +0700

    Create Resindet

--- app/src/main/java/id/ac/umy/unires/sicurezza/NavPage.java
+++ app/src/main/java/id/ac/umy/unires/sicurezza/NavPage.java
@@ -1,13 +1,73 @@
 package id.ac.umy.unires.sicurezza;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 public class NavPage extends AppCompatActivity {
+
+    private BottomNavigationView mMainNav;
+    private FrameLayout mMainFrame;
+
+    private TankkoFragment tankkoFragment;
+    private RankFragment rankFragment;
+    private PointFragment pointFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nav_page);
+        getSupportActionBar().hide();
+
+        mMainFrame = (FrameLayout) findViewById(R.id.main_frame);
+        mMainNav = (BottomNavigationView) findViewById(R.id.bottom_nav);
+
+        tankkoFragment = new TankkoFragment();
+        rankFragment = new RankFragment();
+        pointFragment = new PointFragment();
+
+        mMainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+
+                    case R.id.navtankko :
+                        mMainNav.setItemBackgroundResource(R.color.colorPrimary);
+                        setFragment(tankkoFragment);
+                        return true;
+
+                    case R.id.navrank :
+                        mMainNav.setItemBackgroundResource(R.color.colorAccent);
+                        setFragment(rankFragment);
+                        return true;
+
+                    case R.id.navpoint :
+                        mMainNav.setItemBackgroundResource(R.color.colorPrimaryDark);
+                        setFragment(pointFragment);
+                        return  true;
+
+                        default:
+                            return false;
+
+                }
+            }
+        });
+    }
+
+    private void setFragment(Fragment fragment) {
+
+        FragmentTransaction fragmentTransaction =getSupportFragmentManager().beginTransaction();
+
+        fragmentTransaction.replace(R.id.main_frame, fragment);
+        fragmentTransaction.commit();
+
+
     }
 }
+
+
--- app/src/main/java/id/ac/umy/unires/sicurezza/PageSignIn.java
+++ app/src/main/java/id/ac/umy/unires/sicurezza/PageSignIn.java
@@ -1,7 +1,10 @@
 package id.ac.umy.unires.sicurezza;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PageSignIn extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class PageSignIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_sign_in);
+        getSupportActionBar().hide();
+
+        //Button btnsignin = findViewById(R.id.btnsignin);
+        //btnsignin.setOnClickListener(new View.OnClickListener() {
+            //@Override
+            //public void onClick(View v) {
+                //Intent intent = new Intent(MainActivity.this, PageSignIn.class);
+                //startActivity(intent);
+
+        Button btnsignin2 = findViewById(R.id.button2);
+        btnsignin2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(PageSignIn.this, NavPage.class);
+                startActivity(intent2);
+            }
+        });
     }
 }
