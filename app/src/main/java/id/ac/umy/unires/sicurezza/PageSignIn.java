3b62726b7f5b6e4b81772429b4b3ddde57a159d4
Arifia Kasastra R <arka.progammer@gmail.com>
2019-08-01 20:52:08 +0700

    Take data from EditText and create signingIn method for sign in using Volley

--- app/src/main/java/id/ac/umy/unires/sicurezza/PageSignIn.java
+++ app/src/main/java/id/ac/umy/unires/sicurezza/PageSignIn.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class PageSignIn extends AppCompatActivity {
 
@@ -14,12 +15,10 @@ public class PageSignIn extends AppCompatActivity {
         setContentView(R.layout.activity_page_sign_in);
         getSupportActionBar().hide();
 
-        //Button btnsignin = findViewById(R.id.btnsignin);
-        //btnsignin.setOnClickListener(new View.OnClickListener() {
-            //@Override
-            //public void onClick(View v) {
-                //Intent intent = new Intent(MainActivity.this, PageSignIn.class);
-                //startActivity(intent);
+        EditText etUser = findViewById(R.id.etUsername);
+        EditText etPass = findViewById(R.id.etPassword);
+        final String Username = etUser.getText().toString();
+        final String Password = etPass.getText().toString();
 
         Button btnsignin2 = findViewById(R.id.button2);
         btnsignin2.setOnClickListener(new View.OnClickListener() {
@@ -27,7 +26,12 @@ public class PageSignIn extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent2 = new Intent(PageSignIn.this, NavPage.class);
                 startActivity(intent2);
+                signingIn(Username, Password);
             }
         });
+    }
+
+    private void signingIn(String username, String password) {
+
     }
 }
