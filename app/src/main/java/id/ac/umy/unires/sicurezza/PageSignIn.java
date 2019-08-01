89e1ef085166bad02901b4f451a7cbaf9d84e3c6
Arifia Kasastra R <arka.progammer@gmail.com>
2019-08-01 22:02:45 +0700

    Fix ServerAPI, add empty checker of Username and Password, and use Internet permission

--- app/src/main/java/id/ac/umy/unires/sicurezza/PageSignIn.java
+++ app/src/main/java/id/ac/umy/unires/sicurezza/PageSignIn.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -25,59 +26,66 @@ import static id.ac.umy.unires.sicurezza.utils.ServerAPI.LoginURL;
 public class PageSignIn extends AppCompatActivity {
 
     ProgressDialog progrss;
+    String Username, Password;
+    EditText etUser, etPass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_sign_in);
         getSupportActionBar().hide();
 
-        EditText etUser = findViewById(R.id.etUsername);
-        EditText etPass = findViewById(R.id.etPassword);
-        final String Username = etUser.getText().toString();
-        final String Password = etPass.getText().toString();
+        etUser = findViewById(R.id.etUsername);
+        etPass = findViewById(R.id.etPassword);
+
 
         Button btnsignin2 = findViewById(R.id.button2);
         btnsignin2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Username = etUser.getText().toString();
+                Password = etPass.getText().toString();
                 signingIn(Username, Password);
             }
         });
     }
 
     private void signingIn(final String username, final String password) {
-        ProgressBar();
-        StringRequest request = new StringRequest(Request.Method.POST, LoginURL,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        if(response.equals("Logged In")){
-                            Intent intentLogin = new Intent(PageSignIn.this, NavPage.class);
-                            intentLogin.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-                            startActivity(intentLogin);
-                        } else{
-                            Toaster("Gagal, "+ response);
+        if(TextUtils.isEmpty(username) || TextUtils.isEmpty(password)){
+            Toaster("Usernname atau Password harus diisi");
+        }else{
+            ProgressBar();
+            StringRequest request = new StringRequest(Request.Method.POST, LoginURL,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            if(response.equals("Logged In")){
+                                Intent intentLogin = new Intent(PageSignIn.this, NavPage.class);
+                                intentLogin.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intentLogin);
+                            } else{
+                                Toaster("Gagal, "+ response);
+                            }
+                            progrss.dismiss();
                         }
-                        progrss.dismiss();
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        progrss.dismiss();
-                        Toaster(error.getMessage()!=null?error.getMessage():"");
-                        Log.d("ErrorSignIn", error.getMessage()!=null?error.getMessage():"");
-                    }
-                }){
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                HashMap<String, String> hashMap = new HashMap<>();
-                hashMap.put("username", username);
-                hashMap.put("password", password);
-                return hashMap;
-            }
-        };
-        Volley.newRequestQueue(this).add(request);
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            progrss.dismiss();
+                            Toaster(error.getMessage()!=null?error.getMessage():"");
+                            Log.d("ErrorSignIn", error.getMessage()!=null?error.getMessage():"");
+                        }
+                    }){
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    HashMap<String, String> hashMap = new HashMap<>();
+                    hashMap.put("username", username);
+                    hashMap.put("password", password);
+                    return hashMap;
+                }
+            };
+            Volley.newRequestQueue(this).add(request);
+        }
     }
 
     private void Toaster(String response) {
--- app/src/main/java/id/ac/umy/unires/sicurezza/utils/ServerAPI.java
+++ app/src/main/java/id/ac/umy/unires/sicurezza/utils/ServerAPI.java
@@ -2,10 +2,10 @@ package id.ac.umy.unires.sicurezza.utils;
 
 public class ServerAPI {
     public final static String URL = "https://divman.000webhostapp.com/";
-    public final static String CekStatusKoURL = "cek-status.php";
-    public final static String LoginURL = "proses-login.php";
-    public final static String CekTengKoURL = "cek-tengko.php";
-    public final static String CekResidentURL = "cek-resident.php";
-    public final static String CekDetailResidentURL = "cek-detail.php";
-    public final static String TambahPoinURL = "tambah-poin.php";
+    public final static String CekStatusKoURL = URL+"cek-status.php";
+    public final static String LoginURL = URL+"proses-login.php";
+    public final static String CekTengKoURL = URL+"cek-tengko.php";
+    public final static String CekResidentURL = URL+"cek-resident.php";
+    public final static String CekDetailResidentURL = URL+"cek-detail.php";
+    public final static String TambahPoinURL = URL+"tambah-poin.php";
 }
