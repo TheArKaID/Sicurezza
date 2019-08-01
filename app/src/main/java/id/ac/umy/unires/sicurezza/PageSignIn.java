8cc0c4e9f9c0e06ad0082cfd2a8e003b2c309440
Arifia Kasastra R <arka.progammer@gmail.com>
2019-08-01 21:23:04 +0700

    Add Loading Bar when logging in user

--- app/src/main/java/id/ac/umy/unires/sicurezza/PageSignIn.java
+++ app/src/main/java/id/ac/umy/unires/sicurezza/PageSignIn.java
@@ -1,5 +1,6 @@
 package id.ac.umy.unires.sicurezza;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -23,6 +24,7 @@ import static id.ac.umy.unires.sicurezza.utils.ServerAPI.LoginURL;
 
 public class PageSignIn extends AppCompatActivity {
 
+    ProgressDialog progrss;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,7 +46,7 @@ public class PageSignIn extends AppCompatActivity {
     }
 
     private void signingIn(final String username, final String password) {
-
+        ProgressBar();
         StringRequest request = new StringRequest(Request.Method.POST, LoginURL,
                 new Response.Listener<String>() {
                     @Override
@@ -56,11 +58,13 @@ public class PageSignIn extends AppCompatActivity {
                         } else{
                             Toaster("Gagal, "+ response);
                         }
+                        progrss.dismiss();
                     }
                 },
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
+                        progrss.dismiss();
                         Toaster(error.getMessage()!=null?error.getMessage():"");
                         Log.d("ErrorSignIn", error.getMessage()!=null?error.getMessage():"");
                     }
@@ -78,5 +82,15 @@ public class PageSignIn extends AppCompatActivity {
 
     private void Toaster(String response) {
         Toast.makeText(this, response, Toast.LENGTH_SHORT).show();
+    }
+
+    private void ProgressBar(){
+        if(progrss==null)
+            progrss = new ProgressDialog(this);
+        progrss.setTitle("Logging In");
+        progrss.setMessage("Checking your data");
+        progrss.setCanceledOnTouchOutside(false);
+        progrss.setCancelable(false);
+        progrss.show();
     }
 }
