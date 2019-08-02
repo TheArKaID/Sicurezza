93fb713d49f120358eeefa1796f941bbe8eed57c
Arifia Kasastra R <arka.progammer@gmail.com>
2019-08-02 14:11:36 +0700

    Add logout Function

--- app/src/main/java/id/ac/umy/unires/sicurezza/NavPage.java
+++ app/src/main/java/id/ac/umy/unires/sicurezza/NavPage.java
@@ -1,13 +1,19 @@
 package id.ac.umy.unires.sicurezza;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.Objects;
@@ -29,6 +35,13 @@ public class NavPage extends AppCompatActivity {
 
         mMainFrame = findViewById(R.id.main_frame);
         mMainNav = findViewById(R.id.bottom_nav);
+        ImageView logout = findViewById(R.id.iv_logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Logout();
+            }
+        });
 
         tankkoFragment = new TankkoFragment();
         rankFragment = new RankFragment();
@@ -60,6 +73,35 @@ public class NavPage extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private void Logout() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this)
+                .setTitle("Logout")
+                .setMessage("Anda akan keluar ?")
+                .setPositiveButton("Ya",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                SharedPreferences pref = getApplicationContext().getSharedPreferences("id.ac.umy.unires.sicurezza", MODE_PRIVATE);
+                                SharedPreferences.Editor prefEdit = pref.edit();
+                                prefEdit.putString("username", null);
+                                prefEdit.putString("password", null);
+                                prefEdit.apply();
+
+                                Intent logoutIntent = new Intent(NavPage.this, PageSignIn.class);
+                                logoutIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(logoutIntent);
+                            }
+                        })
+                .setNeutralButton("Batal",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        });
+        builder.show();
     }
 
     private void setFragment(Fragment fragment, String title) {
