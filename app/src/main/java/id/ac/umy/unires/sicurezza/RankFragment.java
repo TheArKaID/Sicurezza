f42c0bcbd55d05f1bff8d7058e96656dd667cf15
Arifia Kasastra R <arka.progammer@gmail.com>
2019-08-03 13:52:21 +0700

    Show data from database

--- app/src/main/java/id/ac/umy/unires/sicurezza/RankFragment.java
+++ app/src/main/java/id/ac/umy/unires/sicurezza/RankFragment.java
@@ -2,12 +2,14 @@ package id.ac.umy.unires.sicurezza;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -15,6 +17,10 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -24,6 +30,8 @@ import id.ac.umy.unires.sicurezza.adapters.PoinAdapter;
 import id.ac.umy.unires.sicurezza.adapters.RankAdapter;
 import id.ac.umy.unires.sicurezza.models.PoinModel;
 import id.ac.umy.unires.sicurezza.models.RankModel;
+
+import static id.ac.umy.unires.sicurezza.utils.ServerAPI.CekRankURL;
 
 public class RankFragment extends Fragment {
 
@@ -35,12 +43,12 @@ public class RankFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_rank, container, false);
 
         loadingBar();
         rankModels = new ArrayList<>();
-        recyclerView = view.findViewById(R.id.poin_recycler);
+        recyclerView = view.findViewById(R.id.fragment_rank);
         recyclerView.setHasFixedSize(true);
         loadRanked();
 
@@ -48,11 +56,28 @@ public class RankFragment extends Fragment {
     }
 
     private void loadRanked() {
-        StringRequest request = new StringRequest(Request.Method.POST, null,
+        StringRequest request = new StringRequest(Request.Method.POST, CekRankURL,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
+                        try {
+                            JSONArray jsonArray = new JSONArray(response);
+                            for(int i = 0; i < jsonArray.length(); i++){
+                                RankModel model = new RankModel();
+                                String object = jsonArray.getString(i);
+                                JSONObject jsonObject = new JSONObject(object);
+                                model.setNama(jsonObject.getString("namaresident"));
+                                model.setPoin(jsonObject.getString("poin"));
+                                model.setUsroh(jsonObject.getString("namausroh"));
+                                rankModels.add(model);
+                            }
 
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(getContext(), "Gagal, " + ((e.getMessage()!=null) ? e.getMessage() : "Coba lagi."), Toast.LENGTH_SHORT).show();
+                        }
+                        adapter();
+                        progress.dismiss();
                     }
                 },
                 new Response.ErrorListener() {
@@ -62,7 +87,7 @@ public class RankFragment extends Fragment {
                     }
                 }){
             @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
+            protected Map<String, String> getParams() {
                 HashMap<String, String> params = new HashMap<>();
                 params.put("idsenior", TankkoFragment.idsenior);
                 return params;
--- app/src/main/java/id/ac/umy/unires/sicurezza/utils/ServerAPI.java
+++ app/src/main/java/id/ac/umy/unires/sicurezza/utils/ServerAPI.java
@@ -1,11 +1,12 @@
 package id.ac.umy.unires.sicurezza.utils;
 
 public class ServerAPI {
-    public final static String URL = "https://divman.000webhostapp.com/";
+    private final static String URL = "https://divman.000webhostapp.com/";
     public final static String CekStatusKoURL = URL+"cek-status.php";
     public final static String LoginURL = URL+"proses-login.php";
     public final static String CekTengKoURL = URL+"cek-tengko.php";
     public final static String CekResidentURL = URL+"cek-resident.php";
     public final static String CekDetailResidentURL = URL+"cek-detail.php";
     public final static String TambahPoinURL = URL+"tambah-poin.php";
+    public final static String CekRankURL = URL+"cek-rank.php";
 }
